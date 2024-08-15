d80f39f407929a936a67a8f31bc173b46058249d
keerthy97 <keerthyy97@gmail.com>
2024-08-15 23:03:24 +0530

    Adding Procfile and requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn countdown_timer.wsgi:application --log-file -
--- countdown_timer/settings.py
+++ countdown_timer/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import django_on_heroku
+import dj_database_url
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +29,7 @@ SECRET_KEY = "django-insecure-v715!(=6j29f8l$nihx35m)9-rtlph&5p(6psugzbi)gv*^ugi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -111,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
+django_on_heroku.settings(locals())
 
 
 # Default primary key field type
